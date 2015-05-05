4ee68aba884ce4479f5e989eb34f965e812b639c
IvanVK <ivan_kotov@abv.bg>
2015-05-06 00:38:21 +0300

    fixed register problem

--- login_form/register.php
+++ login_form/register.php
@@ -3,11 +3,11 @@
 	if(isset($_POST['username']) and isset($_POST['password'])){
 		$username = $_POST['username'];
 		$password = $_POST['password'];
-		$passwordTwo = $_POST['passwordTwo'];
+		$password_two = $_POST['password_two'];
 		$email = $_POST['email'];
 
-		if($password == $passwordTwo){
-			$query = "INSERT INTO logg (username, password, passwordTwo, email) VALUES ('" . $_POST['username'] . "', '" . $_POST['password'] . "', '" . $_POST['passwordTwo'] . "', '" . $_POST['email'] . "'')";
+		if($password == $password_two){
+			$query = "INSERT INTO logg (username, password, password_two, email) VALUES ('" . $_POST['username'] . "', '" . $_POST['password'] . "', '" . $_POST['password_two'] . "', '" . $_POST['email'] . "')";
 			$result = mysql_query($query);
 			echo "TestTestTest";
 			if($result){
